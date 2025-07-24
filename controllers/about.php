26c684132b4a8745b8cbdc6c481443dc15bec9fa
Paul Lourence Calicoy <paullourencecalicoy@gmail.com>
2025-07-24 15:49:58 +0800

    day two

---
+++ controllers/about.php
@@ -0,0 +1,5 @@
+<?php 
+
+$heading = "About Us";
+
+require "view/about.view.php";
---
+++ view/partials/404.php
@@ -0,0 +1,18 @@
+<?php require "view/partials/head.php" ?>
+
+  <?php require "view/partials/nav.php" ?>
+ 
+  <main>
+    <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+        <h1>Sorry. Page Not Found.</h1>
+
+        <p class="mt-4">
+            <a href="/controllers/index.php" class="text-blue underline" >Go Back Home</a>
+        </p>
+
+    </div>
+  </main>
+</div>
+    
+  <?php require "view/partials/footer.php" ?>
+ 
